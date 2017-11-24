8faf602112ad7d84f5693734927c33fae34aee5e
Liang <32484093+Sparkliang@users.noreply.github.com>
2017-11-24 17:20:53 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import time
+import easygui
+
+while 1:
+    easygui.msgbox('休息一下吧^_^', title='休息提醒-L', ok_button='好！')
+    time.sleep(600)
+
+    easygui.msgbox('开始工作了T_T', title='工作提醒-L', ok_button='好吧(╯▽╰)')
+    time.sleep(3000)
